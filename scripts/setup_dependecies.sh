79b862b798c251c22acb7ea2b666a20150ca39bc
Gian Arias <gian.rujano-ext@viavarejo.com.br>
2026-04-01 13:04:10 -0300

    cambio dependecias

--- scripts/setup_dependecies.sh
+++ scripts/setup_dependecies.sh
@@ -5,7 +5,7 @@ set -e
 echo "=== Iniciando instalación OFFLINE de xmltodict ==="
 
 # 1. Definir variables para evitar errores de escritura
-WHEEL_NAME="xmltodict-0.13.0-py2.py3-none-any.whl"
+WHEEL_NAME="xmltodict-1.0.4-py3-none-any.whl"
 BUCKET_PATH="gs://bucket-via-gcb-ia-tributario-hlg-composer/dags/dependencies"
 
 # 2. Copiar el archivo manteniendo su NOMBRE ORIGINAL
